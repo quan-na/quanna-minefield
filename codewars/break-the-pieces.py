e16e89d224d8c257fcb96a214a32fa1c7ad17c19
Nguyen Anh Quan <naquan2112@gmail.com>
2018-02-17 15:06:36 +0700

    starting codewars

---
+++ codewars/break-the-pieces.py
@@ -0,0 +1,12 @@
+USE_BREAK_DISPLAY = True
+
+def break_evil_pieces(shape):
+    pass
+
+# (1)right turn     (1.1)random access       (1.2)pieces from
+#    maniac              grid                     lines
+# (1.3)pieces
+#      inside others
+#
+# (2)one pass    (2.1)connect left    (2.2)connect
+#    parser           or up                conditions
